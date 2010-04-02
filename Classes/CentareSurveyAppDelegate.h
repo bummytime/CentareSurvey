c1a22ec0a41cdbd1e110c7cef6fa07419a0c5deb
Matt Bumgardner <matt@nerddogs.com>
2010-04-02 08:09:04 -0500

    Initial Commit

---
+++ Classes/CentareSurveyAppDelegate.h
@@ -0,0 +1,36 @@
+//
+//  CentareSurveyAppDelegate.h
+//  CentareSurvey
+//
+//  Created by Matthew Bumgardner on 4/2/10.
+//  Copyright Nerddogs, Inc. 2010. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+
+@class RootViewController;
+@class DetailViewController;
+
+@interface CentareSurveyAppDelegate : NSObject <UIApplicationDelegate> {
+    
+    NSManagedObjectModel *managedObjectModel;
+    NSManagedObjectContext *managedObjectContext;	    
+    NSPersistentStoreCoordinator *persistentStoreCoordinator;
+	
+    UIWindow *window;
+
+	UISplitViewController *splitViewController;
+
+	RootViewController *rootViewController;
+	DetailViewController *detailViewController;
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@property (nonatomic, retain) IBOutlet UISplitViewController *splitViewController;
+@property (nonatomic, retain) IBOutlet RootViewController *rootViewController;
+@property (nonatomic, retain) IBOutlet DetailViewController *detailViewController;
+
+@end
